583d4ec6d2c91d0982581355e39d5e76c40a626a
maarioos123 <t8200003@aueb.gr>
2022-01-09 23:56:00 +0200

    Initial commit

---
+++ src/main/java/objects/MovingObject.java
@@ -0,0 +1,51 @@
+package objects;
+
+import java.awt.image.BufferedImage;
+import main_game.Animation;
+import main_game.GamePanel;
+import main_game.Resource;
+
+public abstract class MovingObject extends GameObject {
+    //MovingObject needs Game Panel to spawn on it
+    public BufferedImage[] left;
+    public BufferedImage[] right;
+    double speedx;
+    double speedy;
+
+    Animation rightanimation;
+    Animation leftanimation;
+    //Buffered Images are the ones that contain our main character
+    //they look when they move left,right etc.
+
+    public MovingObject(double worldX, double worldY, int width, int height, String name,
+                        GamePanel gamePanel, double speedx, double speedy) {
+        super(worldX, worldY, width, height, name, gamePanel);
+        this.speedx = speedx;
+        this.speedy = speedy;
+        //While making the object we also determine its speed
+        getMovingObjectImage();
+    }
+
+    public void getMovingObjectImage() {
+        left = Resource.getFilesInDir("src/main/resources/objects/" + name + "/Left");
+        right = Resource.getFilesInDir("src/main/resources/objects/" + name + "/Left");
+        leftanimation = new Animation(0, left);
+        rightanimation = new Animation(0, right);
+    }
+
+    public double getSpeedx() {
+        return speedx;
+    }
+
+    public void setSpeedx(double speedx) {
+        this.speedx = speedx;
+    }
+
+    public double getSpeedy() {
+        return speedy;
+    }
+
+    public void setSpeedy(double speedy) {
+        this.speedy = speedy;
+    }
+}
